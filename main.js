858e8715db40a36acfc7d155511a2b1535d59929
arizkayusril <arizkayusril123@gmail.com>
2019-10-22 14:17:11 +0700

    menambah counting checklist

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Create a new list item when clicking on the "Add" button
+var ul = document.getElementById("myUL");
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -7,15 +8,15 @@ function newElement() {
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
+  ul.appendChild(li);
+  localStorage["list"] = ul.innerHTML
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
+  }
+  document.getElementById("myInput").value = "";
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
@@ -23,6 +24,9 @@ function newElement() {
       div.style.display = "none";
     }
   }
+}
+if (localStorage["list"]) {
+  ul.innerHTML = localStorage["list"];
 }
 
 // Create a "close" button and append it to each list item
@@ -48,9 +52,16 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
+var number = document.getElementById("number");
+count = 0
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
+    ev.target.tagName == 'number'
+    count ++;
+    number.innerHTML = " " + count;
+  } else {
+    count --;
+    number.innerHTML = " " + count;
   }
 }, false);
-
